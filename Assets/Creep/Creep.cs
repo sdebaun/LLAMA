cc5521e2297d56a8df5670da0ae931d3bbdcbae6
Mike Bott <mbott@tripwestgames.com>
2015-09-29 23:05:51 -0700

    Add a creep spawner to the miey scene

--- Assets/Creep/Creep.cs
+++ Assets/Creep/Creep.cs
@@ -4,16 +4,20 @@ using System.Collections;
 public class Creep : MonoBehaviour {
 
     public Transform goal;
+    public float nav_wait;
 
     NavMeshAgent agent;
 
     // Use this for initialization
     void Start () {
         agent = GetComponent<NavMeshAgent>();
-        agent.destination = goal.position;
+        GameObject goalGameObject = GameObject.Find("Goal");
+        agent.destination = goalGameObject.transform.position;
 	}
+
+
 	
-	// Update is called once per frame
+    // Update is called once per frame
 	void Update () {
 	    
 	}
