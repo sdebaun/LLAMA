0e4083b62d2bc6441fa8584b8ef218a2aab270fc
Mike Bott <mbott@tripwestgames.com>
2015-09-29 18:49:20 -0700

    Add Basic Creep and NavMesh backstage
    Creep moves to Goal

---
+++ Assets/Creep/Creep.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Creep : MonoBehaviour {
+
+    public Transform goal;
+
+    NavMeshAgent agent;
+
+    private Transform target;
+
+    // Use this for initialization
+    void Start () {
+        target = goal;
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
