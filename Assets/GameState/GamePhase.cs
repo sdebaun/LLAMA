f2074ac32240518443fe9cf7d924bf5be1630ef3
Stevo <sdebaun@sparks.network>
2015-09-30 17:00:27 -0700

    state updates on connect after game start

--- Assets/GameState/GamePhase.cs
+++ Assets/GameState/GamePhase.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 using System.Collections;
 
@@ -6,33 +7,76 @@ public delegate void PhaseChangeEventHandler(GamePhase phase);
 
 public class GamePhase : NetworkBehaviour {
 
-    public event PhaseChangeEventHandler changeListeners;
-
     public GameObject dayPhaseTitle;
+    public Text dayPhaseTimer;
     public GameObject nightPhaseTitle;
+    public Text nightPhaseCreepCounts;
+    public int secondsPerDay;
 
     [SyncVar(hook="OnPhaseChange")]
     public string phase;
-
+    public event PhaseChangeEventHandler changeListeners;
     private void OnPhaseChange(string p) {
-        if (changeListeners != null) { changeListeners(this); }
-        Application.UnloadLevel("ReadyRoomOverlay");
-        bool isDay = (p == "day");
-        dayPhaseTitle.SetActive(isDay);
-        nightPhaseTitle.SetActive(!isDay);
+        if (p=="day" || p=="night") {
+            if (changeListeners != null) { changeListeners(this); } // not being used atm; how i *should* do it
+            Application.UnloadLevel("ReadyRoomOverlay");
+            bool isDay = (p == "day");
+            dayPhaseTitle.SetActive(isDay);
+            nightPhaseTitle.SetActive(!isDay);
+        }
     }
+
+    [SyncVar(hook = "OnSecondsChange")]
+    public int secondsLeft;
+    private void OnSecondsChange(int s) {
+        if (secondsLeft != null) {
+            dayPhaseTimer.text = secondsToFormattedTime(s);
+        }
+    }
+
+    public override void OnStartClient() {
+        OnPhaseChange(phase);
+        OnSecondsChange(secondsLeft);
+    }
+    //void Start() {
+    //    OnPhaseChange(phase);
+    //    OnSecondsChange(secondsLeft);
+    //}
 
     [Command]
     public void CmdSwitchTo(string newPhase) {
-        phase = newPhase;
+        SwitchTo(newPhase);
     }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
+    public void SwitchTo(string p) {
+        phase = p;
+        if (p == "day") { StartDay(); } else { StartNight(); }
+    }
+
+    IEnumerator CountDown() {
+        while (secondsLeft>0) {
+            yield return new WaitForSeconds(1);
+            secondsLeft -= 1;
+        }
+        SwitchTo("night");
+    }
+
+    private void StartDay() {
+        secondsLeft = secondsPerDay;
+
+        StartCoroutine(CountDown());
+    }
+    private void StartNight() {
+    }
+
+    private string secondsToFormattedTime(int s) {
+        string f = "";
+        if (s > 60) { f += (int)(s / 60); } else { f += "0:"; }
+        int ss = s % 60;
+        if (ss < 10) { f += "0"; }
+        f += ss;
+        return f;
+    }
+
+
 }
