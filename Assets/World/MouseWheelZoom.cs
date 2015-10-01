e30a94150d2439493c2fc3dbee16403fd1359863
Stevo <sdebaun@sparks.network>
2015-10-01 12:52:58 -0700

    mousewheelzoom broh.

---
+++ Assets/World/MouseWheelZoom.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseWheelZoom : MonoBehaviour {
+
+	public float maxOrthographicSize = 6;
+	public float minOrthographicSize = 1;
+	public float zoomInFactor = 0.9f;
+	public float zoomOutFactor = 1.1f;
+
+	void Update () {
+		float mouseScroll = Input.GetAxis ("Mouse ScrollWheel");
+		if (mouseScroll < 0) {
+			Camera.main.orthographicSize = Mathf.Min (Camera.main.orthographicSize * zoomOutFactor, maxOrthographicSize);
+		} else if (mouseScroll > 0) {
+			Camera.main.orthographicSize = Mathf.Max (Camera.main.orthographicSize * zoomInFactor, minOrthographicSize);
+		}
+	}
+}
