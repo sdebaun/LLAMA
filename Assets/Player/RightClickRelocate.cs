003eff45ec40d3d308f22404dbcbe43ba74c6f77
Stevo <sdebaun@sparks.network>
2015-10-01 18:50:54 -0700

    networked movetargets and moving

--- Assets/Player/RightClickRelocate.cs
+++ Assets/Player/RightClickRelocate.cs
@@ -4,22 +4,48 @@ using System.Collections;
 
 public class RightClickRelocate : NetworkBehaviour {
 
+    public GameObject moveGoalPrefab;
+
     public GameObject moveGoalObject;
 
+    public float stopDistance = 0.1f;
+
     private NavMeshAgent agent;
+    private PlayerModel player;
 
 	void Start () {
-        agent = GetComponent<NavMeshAgent>();
-        moveGoalObject.transform.SetParent(null);
-	}
+        if (isServer) {
+            player = GetComponent<PlayerModel>();
+            agent = GetComponent<NavMeshAgent>();
+            agent.stoppingDistance = stopDistance;
+            Debug.Log("Creating moveGoalObject on server");
+            moveGoalObject = Instantiate(moveGoalPrefab) as GameObject;
+            NetworkServer.Spawn(moveGoalObject);
+            moveGoalObject.SetActive(false);
+            moveGoalObject.GetComponent<SpriteRenderer>().color = player.color;
+        }
+
+    }
 	
 	void Update () {
 	    if (isLocalPlayer) {
             if (Input.GetMouseButtonUp(1)) {
                 Vector3 mPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                moveGoalObject.transform.position = new Vector3(mPos.x, 0f, mPos.z);
-                agent.destination = moveGoalObject.transform.position;
+                CmdMoveTarget(new Vector3(mPos.x, 0f, mPos.z));
+            }
+        }
+        if (isServer && moveGoalObject.activeSelf) {
+            if (Vector3.Distance(moveGoalObject.transform.position, transform.position) <= stopDistance) {
+                moveGoalObject.GetComponent<ServerDeactivate>().ServerSetActive(false);
             }
         }
 	}
+
+    [Command]
+    public void CmdMoveTarget(Vector3 p) {
+        moveGoalObject.GetComponent<ServerDeactivate>().ServerSetActive(true);
+        moveGoalObject.transform.position = p;
+        agent.destination = p;
+    }
+
 }
