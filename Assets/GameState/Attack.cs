b3d5863d6ae9a9b987179a6c28e90b3e077a3c9c
Stevo <sdebaun@sparks.network>
2015-10-02 13:14:29 -0700

    collider trigger targeting

---
+++ Assets/GameState/Attack.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+
+    public bool hasTarget = false;
+    public GameObject target;
+
+    public float maxLockDistance = 6f;
+
+    void Update() {
+        if ((target == null) || (distanceToTarget()> maxLockDistance) ) { clearTarget(); }
+    }
+
+    public float distanceToTarget() {
+        return target ? Vector3.Distance(transform.position, target.transform.position) : 0f;
+    }
+
+    public void setTarget(GameObject g) {
+        hasTarget = true;
+        target = g;
+    }
+
+    public void clearTarget() {
+        hasTarget = false;
+        target = null;
+    }
+
+}
---
+++ Assets/GameState/TriggerTarget.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerTarget : MonoBehaviour {
+
+    public int targetLayerID = 8;
+
+    public Attack attack;
+
+    public void OnTriggerEnter(Collider other) {
+        Debug.Log("Triggered by " + other.gameObject.name + " layer " + other.gameObject.layer);
+        if (!attack.hasTarget && (other.gameObject.layer == targetLayerID)) { attack.setTarget(other.gameObject); }
+    }
+
+}
