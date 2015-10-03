a1cf2b10a137ab24ee3be0749e18c7f10543836f
Stevo <sdebaun@sparks.network>
2015-10-02 22:28:27 -0700

    the cc blows up & shows health networked

--- Assets/ColonyCenter/ColonyCenter.cs
+++ Assets/ColonyCenter/ColonyCenter.cs
@@ -1,18 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 using System.Collections;
 
 public class ColonyCenter : NetworkBehaviour {
 
+    public Text ccHealth;
+
+    private Damageable damageable;
+
 	// Use this for initialization
 	void Start () {
-	
+        damageable = GetComponent<Damageable>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (isClient) { ccHealth.text = "" + (int)damageable.currentHealth; }
+    }
 
     void OnDestroy() {
         Application.LoadLevel("Menu"); // rocks fall, everyone dies
