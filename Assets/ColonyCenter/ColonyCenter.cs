a06806e3b4c4995f55143c2b4bdfd14fe5b4268d
Stevo <sdebaun@sparks.network>
2015-10-02 21:59:31 -0700

    cc can take damage

---
+++ Assets/ColonyCenter/ColonyCenter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class ColonyCenter : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnDestroy() {
+        Application.LoadLevel("Menu"); // rocks fall, everyone dies
+    }
+
+}
