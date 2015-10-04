23f251ac6a73dd9ce59374281c1949b26ada3941
Stevo <sdebaun@sparks.network>
2015-10-04 14:30:53 -0700

    still working
    added new component direct to prefab

--- Assets/World/PopulateAll.cs
+++ Assets/World/PopulateAll.cs
@@ -4,19 +4,19 @@ using System.Collections;
 
 public class PopulateAll : NetworkBehaviour {
 
-    public override void OnStartServer() {
-        //NetworkServer.SpawnObjects();
-        foreach (NetworkIdentity p in GetComponentsInChildren<NetworkIdentity>()) {
-            NetworkServer.Spawn(p.gameObject);
-        }
-    }
+    //public override void OnStartServer() {
+    //    //NetworkServer.SpawnObjects();
+    //    foreach (NetworkIdentity p in GetComponentsInChildren<NetworkIdentity>()) {
+    //        NetworkServer.Spawn(p.gameObject);
+    //    }
+    //}
 
-    void Start() {
-        if (isServer) {
-            foreach (SelfPopulator p in GetComponentsInChildren<SelfPopulator>()) {
-                p.Populate();
-            }
-        }
-    }
+    //void Start() {
+    //    if (isServer) {
+    //        foreach (SelfPopulator p in GetComponentsInChildren<SelfPopulator>()) {
+    //            p.Populate();
+    //        }
+    //    }
+    //}
 
 }
