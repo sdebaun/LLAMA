5b56c1c7e6eabb733cb87b535dc1023ce66e3b3f
Stevo <sdebaun@sparks.network>
2015-10-03 19:58:02 -0700

    cc ui now uses UITextUpdater component instead

--- Assets/ColonyCenter/ColonyCenter.cs
+++ Assets/ColonyCenter/ColonyCenter.cs
@@ -5,20 +5,6 @@ using System.Collections;
 
 public class ColonyCenter : NetworkBehaviour {
 
-    public Text ccHealth;
-
-    private Damageable damageable;
-
-	// Use this for initialization
-	void Start () {
-        damageable = GetComponent<Damageable>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (isClient) { ccHealth.text = "" + (int)damageable.currentHealth; }
-    }
-
     void OnDestroy() {
         GameObject.Find("NetworkReference").GetComponent<NetworkReference>().disconnect(); // rocks fall, everyone dies
     }
---
+++ Assets/UI/UITextUpdater.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Reflection;
+
+public class UITextUpdater : MonoBehaviour {
+    public GameObject sourceGameObject; // in editor or WatchSource() at runtime
+    public string sourceComponentName = "Damageable";
+    public string sourceFieldName = "currentHealth";
+    public bool floatToWholeNumber = true;
+
+    private Text destText;
+    private FieldInfo sourceFieldInfo;
+    private Component sourceComponent;
+
+    void Start() {
+        destText = GetComponent<Text>();
+        StartWatching(sourceGameObject);
+    }
+
+    void Update() {
+        if (sourceGameObject && destText) destText.text = GetSourceText();
+    }
+
+    private string GetSourceText() {
+        object val = sourceFieldInfo.GetValue(sourceComponent);
+        return floatToWholeNumber ? ((float)val).ToString("N0") : val.ToString();
+    }
+
+    public void StartWatching(GameObject go, string field = null) {
+        if (!go) {
+            sourceGameObject = null;  sourceComponent = null;  sourceFieldInfo = null;
+            return;
+        }
+        if (field!=null) sourceFieldName = field;
+        sourceGameObject = go;
+        sourceComponent = sourceGameObject.GetComponent(sourceComponentName);
+        sourceFieldInfo = sourceComponent.GetType().GetField(sourceFieldName);
+    }
+
+}
