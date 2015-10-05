6e3ef0f56c573ce60bf4b02e708fbd3010824c9e
Stevo <sdebaun@sparks.network>
2015-10-05 10:21:22 -0700

    moar refactor, started using event system

---
+++ Assets/ClickHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ClickHandler : MonoBehaviour {
+
+    public GameObject prefab;
+
+	public void DoThing(BaseEventData e) {
+        PointerEventData p = e as PointerEventData;
+        //p.pointerPressRaycast.worldPosition;
+
+        Vector3 worldPosition = p.pointerPressRaycast.worldPosition;
+        //worldPosition.y = 0;
+        Debug.Log("mouse button " + (p as PointerEventData).button + " at screen " + p.position + " world " + worldPosition);
+        GameObject g = Instantiate(prefab, worldPosition, Quaternion.identity) as GameObject;
+        NetworkServer.Spawn(g);
+
+    }
+}
