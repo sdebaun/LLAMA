de0a6b9cc81e33f21dae994ccd3444c22fee56d4
Stevo <sdebaun@sparks.network>
2015-10-05 12:36:27 -0700

    wip click handling on ground

---
+++ Assets/Player/PlayerClickHandler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class PlayerClickHandler : MonoBehaviour {
+
+    public PlayerModel localPlayer;
+
+    public void HandleEvent(BaseEventData e) {
+        if (localPlayer != null) localPlayer.HandlePointerEvent(e as PointerEventData);
+    }
+}
--- Assets/Player/PlayerModel.cs
+++ Assets/Player/PlayerModel.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.EventSystems;
 using System.Collections;
 
 public class PlayerModel : NetworkBehaviour {
@@ -10,11 +11,28 @@ public class PlayerModel : NetworkBehaviour {
     private PlayerListControl players;
 
     public override void OnStartAuthority() {
-        Debug.Log("Starting PlayerModel on Authority");
+        Debug.Log("PlayerModel.OnStartAuthority (should be server)");
         color = newRandomColor();
         gameObject.GetComponent<ServerDriven>().SetMeshColor(color);
     }
 
+    public override void OnStartLocalPlayer() { // connect ui/camera/etc to this thing
+        Debug.Log("PlayerModel.OnStartLocalPlayer");
+        GameObject ground = GameObject.Find("Ground"); // brittle
+        if (ground!=null) ground.GetComponent<PlayerClickHandler>().localPlayer = this;
+    }
+
+    [Client]
+    public void HandlePointerEvent(PointerEventData p) {
+        Vector3 worldPosition = p.pointerPressRaycast.worldPosition;
+        //worldPosition.y = 0;
+        Debug.Log("mouse button " + p.button + " at screen " + p.position + " world " + worldPosition);
+        if (p.button == PointerEventData.InputButton.Left) {
+        } else if (p.button == PointerEventData.InputButton.Right) {
+        }
+    }
+
+    [Server]
     private Color newRandomColor() {
         float[] c = { Random.Range(0.3f, 0.5f), Random.Range(0.3f, 0.5f), Random.Range(0.3f, 0.5f) };
         c[Random.Range(0, 3)] += Random.Range(0.4f, 0.5f);
@@ -22,7 +40,7 @@ public class PlayerModel : NetworkBehaviour {
     }
 
     void Start () {
-        Debug.Log("Starting PlayerModel");
+        Debug.Log("PlayerModel.Start");
         // gotta be a better way to do this
         GameObject go = GameObject.Find("PlayerList");
         players = go.GetComponent<PlayerListControl>();
