a12b905533af05fa058e0f4fd1e6572120298a45
Stevo <sdebaun@sparks.network>
2015-10-05 21:46:59 -0700

    network followcam

--- Assets/Player/FollowCam.cs
+++ Assets/Player/FollowCam.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 
 public class FollowCam : MonoBehaviour {
-    public float scrollSpeed = 1f;
 
 	void Update () {
         Camera.main.transform.position = new Vector3(transform.position.x, Camera.main.transform.position.y, transform.position.z);
