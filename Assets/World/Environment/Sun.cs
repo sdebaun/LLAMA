7929b73a07b291b593c32aa959af95ada2b44998
Mike Bott <mbott@tripwestgames.com>
2015-10-06 19:20:12 -0700

    Working Sun. Prefab probably not necessary.

--- Assets/World/Environment/Sun.cs
+++ Assets/World/Environment/Sun.cs
@@ -3,13 +3,33 @@ using System.Collections;
 
 public class Sun : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    public bool isRisen = false; // a risen sun moves across the sky until the rotation is >180 or <0
+    public float daySeconds = 3;
+    public float curTime = 0;
+
+    private Light theSun;
+    private float theAngle;
+
+    void Start () {
+        theSun = GetComponent<Light>();
+    }
+
+    public void Rise(float secondsToSet) {
+        daySeconds = secondsToSet;
+        isRisen = true;
+    }
+
 	// Update is called once per frame
 	void Update () {
-	
+	    if (isRisen) {
+            // Set the rotation mapping the range 0-180 degrees X rotation to the range 0-daySeconds seconds
+            theAngle = 180 * (daySeconds - curTime) / daySeconds;
+            theSun.transform.rotation = Quaternion.Euler(theAngle, 90, 0);
+            curTime += Time.deltaTime;
+            if (curTime > daySeconds) { // Sun has set
+                curTime = 0;
+                isRisen = false;
+            }
+        }
 	}
 }
