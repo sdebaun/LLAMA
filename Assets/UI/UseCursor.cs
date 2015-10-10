4a1f1d4252f55dfc83142e732d2305c168474e9e
Stevo <sdebaun@sparks.network>
2015-10-10 11:16:25 -0700

    added cursor why i dunno

---
+++ Assets/UI/UseCursor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class UseCursor : MonoBehaviour {
+
+    public Texture2D cursorTexture;
+
+    public void Activate() {
+        print("Using custom cursor");
+        Cursor.SetCursor(cursorTexture, new Vector2(16,16), CursorMode.ForceSoftware);
+    }
+
+    public void Deactivate() {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}
+
