e4c454f124ced510c43e7fe07237b1cbc03d2e4a
Stevo <sdebaun@sparks.network>
2015-10-13 22:25:33 -0700

    switched hotkey to horizontal layout

--- Assets/UI/PlayerRowControl.cs
+++ Assets/UI/PlayerRowControl.cs
@@ -7,6 +7,7 @@ public class PlayerRowControl : MonoBehaviour {
     public Text nameLabel;
     public List<Image> coloredImages = new List<Image>();
 
+    public bool textColor = true;
 
     private PlayerModel player;
     private Color color;
@@ -14,7 +15,7 @@ public class PlayerRowControl : MonoBehaviour {
     public void setPlayer(PlayerModel p) {
         player = p;
         color = player.gameObject.GetComponent<RandomColor>().color;
-        nameLabel.color = color;
+        if (textColor) nameLabel.color = color;
         foreach (Image i in coloredImages) { i.color = color; }
     }
 
