ff0a220b32ec682810d97fbd92eba5fb27f6e9d7
Stevo <sdebaun@sparks.network>
2015-10-19 19:13:47 -0700

    little plastic houses

---
+++ Assets/Entity/HouseBuilder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+/*
+Hooks up to events that 
+*/
+public class HouseBuilder : NetworkBehaviour {
+
+    public List<GameObject> houses = new List<GameObject>();
+
+    public void OnSize(int newSize) {
+        for (int i=0;i<newSize;i++) {
+            houses[i].SetActive(true);
+        }
+    }
+}
