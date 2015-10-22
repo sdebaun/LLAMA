64324e7cd80d0a21bba3d2c47fa3cf28295c75c3
Zack Kirsch <zack@qumulo.com>
2015-10-21 18:05:14 -0700

    Create SampleTest that uses NUnit.
    Tests apparently have to be in an Editor folder, see link for more
    details:
    
    http://answers.unity3d.com/questions/636988/the-type-or-namespace-name-n
    unit-cannot-be-foundty.html

---
+++ Assets/Editor/Test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+
+public class MyTest : MonoBehaviour {
+	
+	public class SampleTest
+	{
+		[Test]
+		public void TestThatSucceeds()
+		{
+			Assert.That(2 + 2 == 4);
+		}
+
+		[Test]
+		public void TestThatFails()
+		{
+			Assert.That (2 + 2 == 42);
+		}
+	}
+}
